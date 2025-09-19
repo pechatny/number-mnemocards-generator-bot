4f1f6da6f7cc3c17924dcc8edf5b6b9c81868acc
Dmitry Pechatnikov <pechatny@gmail.com>
2025-09-19 19:40:50 +0300

    Создал скрипт сборки и публикации приложения в dockerhub

---
+++ build-and-publish-to-dockerhub.ps1
@@ -0,0 +1,33 @@
+﻿# build-and-publish-to-dockerhub.ps1
+param(
+    [string]$Version = "2.0.4",
+    [string]$AppName = "number-mnemocards-generator-bot"
+)
+
+$Tag = "${AppName}:$Version"
+$DockerHubTag = "pechatny/${AppName}:$Version"
+$DockerHubTagLatest = "pechatny/${AppName}:latest"
+$OutputFile = "${AppName}-${Version}.tar"
+
+Write-Host "Сборка Docker образа..." -ForegroundColor Green
+docker build -t $Tag .
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Ошибка сборки Docker образа!" -ForegroundColor Red
+    exit 1
+}
+
+Write-Host "Присваиваю публичных тегов новой версии образа..." -ForegroundColor Green
+docker tag $Tag $DockerHubTag
+docker tag $Tag $DockerHubTagLatest
+
+Write-Host "Отправка образа ${DockerHubTag} в dockerhub..." -ForegroundColor Green
+docker push ${DockerHubTag}
+
+Write-Host "Отправка образа ${DockerHubTagLatest} в dockerhub..." -ForegroundColor Green
+docker push ${DockerHubTagLatest}
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Ошибка экспорта образа!" -ForegroundColor Red
+    exit 1
+}
