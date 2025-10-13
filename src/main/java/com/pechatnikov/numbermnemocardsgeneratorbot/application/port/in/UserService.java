a5102887d8382bcc87e896db31b276e61c0233d3
Dmitry Pechatnikov <pechatny@gmail.com>
2025-10-13 22:03:42 +0300

    UserService

--- src/main/java/com/pechatnikov/numbermnemocardsgeneratorbot/application/port/in/UserService.java
+++ src/main/java/com/pechatnikov/numbermnemocardsgeneratorbot/application/port/in/UserService.java
@@ -2,6 +2,9 @@ package com.pechatnikov.numbermnemocardsgeneratorbot.application.port.in;
 
 import com.pechatnikov.numbermnemocardsgeneratorbot.domain.user.User;
 
+import java.util.Optional;
+
 public interface UserService {
     User getOrCreateUser(GetOrCreateUserCommand getOrCreateUserCommand);
+    Optional<User> findByTelegramId(Long telegramId);
 }
